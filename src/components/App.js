19e2f7b90c45397d9867c882eb45153e3e5e536d
AMAYAJIS <109139504+AMAYAJIS@users.noreply.github.com>
2022-08-05 19:55:36 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,8 @@
 import React, { Component, useState } from "react";
+import Heading from "./components/Heading";
+import InputQuery from "./components/InputQuery";
+import SubHeading from "./components/SubHeading";
+import SubmitButton from "./components/SubmitButton ";
 import '../styles/App.css';
 
 const App = () => {
